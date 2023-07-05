44664bdfefdbacce87262e755c3ee6419ccde410
Alexis <lexisgasa3007@gmail.com>
2023-07-05 23:22:22 +0200

    methods

--- Curso JS Moderno/12-ArrayMethods/js/04-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/04-app.js
@@ -8,3 +8,18 @@ const carrito = [
   { nombre: "Teclado", precio: 400 },
   { nombre: "Celular", precio: 700 },
 ];
+
+
+// el metodo filter es muy util ya que devuelve true en base a la condicion que se establece, y crea un array con los elementos que pasan dicha condicion
+
+let resultado;
+
+resultado = carrito.filter( producto => producto.precio > 400 )
+
+console.log(resultado);
+
+// el filter tambien se usa para quitar un elemento en concreto, ya que el return es un array nuevo, se pueden coger TODOS los elementos que componen ese array menos el que haga match
+
+console.log(carrito.filter( item => item.nombre !== "Teclado"))
+
+
